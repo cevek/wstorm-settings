08b7091eb4c9d3a791ce48d8a189761d78eb961a
cody <cevek@ya.ru>
2016-11-02 15:30:37 +0500

    WS-163.6512.13 <cody@macbook.local Create IntelliLang.xml, project.default.xml, diff.xml, AngularJS .xml, file.template.settings.xml, find.xml, code.style.schemes.xml, Dart.xml, Zen HTML.xml, path.macros.xml, Darcula copy.icls, github_settings.xml, hg.xml, Default.xml, JavaScript.xml, AngularJS.xml, Default _2_.xml, duplocatorSettings.xml, Zen XSL.xml, surround.xml, editor.codeinsight.xml, keymap.xml, editor.xml, applicationLibraries.xml, webServers.xml, debugger.xml, baseRefactoring.xml, mine.icls, ide.general.xml, Mac OS X copy 0.xml, user.xml, git.xml, other.xml, scratches.xml, Zen CSS.xml, xpath.xml, ui.lnf.xml, filetypes.xml, xsl.xml, watcherDefaultTasks.xml, colors.scheme.xml, html_xml.xml, notifications.xml, customization.xml, TypeScript File.ts, phonegap.xml, laf.xml, Default _1_.xml, web-browsers.xml, abbreviations.xml, vcs.xml, TS ReactComponent.tsx, usageView.xml, Mac OS X copy.xml

---
+++ fileTemplates/TS ReactComponent.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './${NAME}.css';
+
+export class ${NAME} extends React.Component<{}, {}>{
+    render() {
+        return <div className="${NAME}">
+        
+        </div>;     
+    }
+}
